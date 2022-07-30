ce383df1b423aeb9022b8de722939efa3b95c023
Paul Beck <paulbeck@Pauls-MacBook-Pro.local>
2022-07-30 17:15:51 +0200

    logout on 401s

--- src/services/backend/getData.js
+++ src/services/backend/getData.js
@@ -1,12 +1,23 @@
 import Axios from "axios";
 import getAdminHeaders from "../utils/getAdminHeaders";
-import logout_if_response_401 from "../utils/logout_if_response_401";
+import logout from "../utils/logout_if_response_401";
+
+Axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      logout();
+    }
+    return error;
+  }
+);
 
 export default async function getData(url) {
   const headers = getAdminHeaders();
   try {
     const response = await Axios.get(url, { headers });
-    logout_if_response_401(response);
     return response.data;
   } catch (err) {
     console.log(err);
--- src/services/utils/logout_if_response_401.js
+++ src/services/utils/logout_if_response_401.js
@@ -1,6 +1,4 @@
-export default function logout_if_response_401(response) {
-  if (response.status === 401) {
-    window.localStorage.removeItem("authToken");
-    window.location.href = "/";
-  }
+export default function logout() {
+  window.localStorage.removeItem("authToken");
+  window.location.href = "/";
 }
